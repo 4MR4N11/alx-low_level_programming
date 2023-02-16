596581757e75efc72146429a72baa4d44857da32
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-02-16 03:56:53 -0800

    Mandatory part

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * print the message using puts function.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * print the message using printf function.
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

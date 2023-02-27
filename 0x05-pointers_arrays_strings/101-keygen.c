a4d215a80ee1173eb1f7e47410f7875243dfb48f
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-02-27 09:30:26 -0800

    Advanced part

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,13 +1,49 @@
 #include "main.h"
 
 /**
- * main - Entry point.
- * Return: 0 (SUCCESS)
+ * len_counter - count each character.
+ * @pass: pointer to string.
+ * Return: length.
+ */
+static int len_counter(char *pass)
+{
+	int i;
+	int len;
+
+	len = 0;
+	i = 0;
+	while (pass[i])
+	{
+		len = len + pass[i];
+		i++;
+	}
+	return (len);
+}
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
+	int i = 0;
+	int len;
+	char pass[30];
+	char chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
 	srand(time(0));
-	printf("%d", rand());
+	while (len != 2772)
+	{
+		len = 0;
+		i = 0;
+		while (i < 30)
+		{
+			pass[i] = chars[rand() % 52];
+			i++;
+		}
+		len = len_counter(pass);
+	}
+	printf("%s\n", pass);
 	return (0);
 }

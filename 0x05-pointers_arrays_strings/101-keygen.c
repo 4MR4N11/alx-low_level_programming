2a90ee26bb7176da25da43632ee5e1acab5ae37c
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-02-27 07:00:31 -0800

    Advanced part

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point.
+ * Return: 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	srand(time(0));
+	printf("%d", rand());
+	return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);

bb47aefba63466f33c2322ff52bd5151f494e48c
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-03-01 07:31:57 -0800

    Mandatory par

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,13 +10,13 @@
 
 static int check_previous(char *str, int i)
 {
-	if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t'
-		|| str[i - 1] == '\n' || str[i - 1] == ','
-		|| str[i - 1] == ';' || str[i - 1] == '.'
-		|| str[i - 1] == '!' || str[i - 1] == '?'
-		|| str[i - 1] == '"' || str[i - 1] == '{'
-		|| str[i - 1] == '}' || str[i - 1] == ')'
-		|| str[i - 1] == '(')
+	if (i == 0 || str[i - 1] == 32 || str[i - 1] == 9
+		|| str[i - 1] == 10 || str[i - 1] == 44
+		|| str[i - 1] == 59 || str[i - 1] == 46
+		|| str[i - 1] == 33 || str[i - 1] == 63
+		|| str[i - 1] == 34 || str[i - 1] == 40
+		|| str[i - 1] == 123 || str[i - 1] == 41
+		|| str[i - 1] == 124)
 	{
 		return (1);
 	}
@@ -36,8 +36,8 @@ char *cap_string(char *str)
 	i = 0;
 	while (str[i])
 	{
-		if (check_previous(str, i) && ((str[i] <= 'z') && (str[i] >= 'a')))
-				str[i] -= 32;
+		if ((check_previous(str, i)) && ((str[i] >= 'a') && (str[i] <= 'z')))
+			str[i] -= 32;
 		i++;
 	}
 	return (str);

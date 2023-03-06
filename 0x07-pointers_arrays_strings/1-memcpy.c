8d989a684805a90fb9780dada5c68bd84bab2433
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-03-06 05:41:25 -0800

    Mandatory part

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: number of bytes to copy.
+ * Return: @dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #endif

087907948f4902dbd932c6bd48a1d5d08f83400c
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-03-06 05:36:28 -0800

    Mandatory part

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,10 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
-	i = -1;
-	while (++i < n)
-		s[i] = b;
+	i = 0;
+	while (i < n)
+		s[i++] = b;
 	return (s);
 }

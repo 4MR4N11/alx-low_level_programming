4831d87826a6c173a9d9bb65bee11243b01a098c
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-03-10 07:10:51 -0800

    Mandatory part

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,99 +1,22 @@
 #include "main.h"
 
 /**
- * sign_count - count how many '-' to determine the sign of the number.
- * @s: pointer to the string.
- * Return: number of '-'.
+ * checknum - check if it's not a number.
+ * @s: pointer to string(number).
+ * Return: 0 @s is number, 1 if not.
  */
 
-static int sign_count(char *s)
-{
-	int i;
-	int sign;
-
-	i = 0;
-	sign = 0;
-	while (s[i] && (s[i] > '9' || s[i] < '0'))
-	{
-		if (s[i] == '-')
-			sign++;
-		i++;
-	}
-	return (sign);
-}
-
-/**
- * check_string - checks if there is no number in the string.
- * @s: pointer to string.
- * Return: 0 if there is a number, 1 if not.
- */
-
-static int check_string(char *s)
+int checknum(char *s)
 {
 	int i;
 
-	i = 0;
-	while (s[i])
+	i = -1;
+	while (s[++i])
 	{
-		if (s[i] <= '9' && s[i] >= '0')
-			return (0);
-		i++;
+		if (!isdigit(s[i]))
+			return (1);
 	}
-	return (1);
-}
-
-/**
- * conv - convert to integer.
- * @s: pointer to string.
- * @i: index of where the digit found in the string.
- * @sign: sign count.
- * Return: the integer.
- */
-
-static int conv(char *s, int i, int sign)
-{
-	int num;
-
-	num = 0;
-	while (s[i] <= '9' && s[i] >= '0')
-	{
-		if (sign % 2 != 0)
-			num += (s[i] - '0') * -1;
-		else
-			num += s[i] - '0';
-		if ((s[i + 1] <= '9') && (s[i + 1] >= '0'))
-			num *= 10;
-		i++;
-	}
-	return (num);
-}
-
-/**
- * _atoi - convert a string to an integer.
- * @s: pointer to string.
- * Return: the integer.
- */
-
-int _atoi(char *s)
-{
-	int sign;
-	int i;
-	int num;
-
-	if (check_string(s))
-		return (-1);
-	sign = sign_count(s);
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] <= '9' && s[i] >= '0')
-		{
-			num = conv(s, i, sign);
-			break;
-		}
-		i++;
-	}
-	return (num);
+	return (0);
 }
 
 /**
@@ -117,12 +40,12 @@ int main(int ac, char **av)
 	}
 	while (av[i])
 	{
-		if (_atoi(av[i]) == -1 || _atoi(av[i]) < 0)
+		if (checknum(av[i]))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		res += _atoi(av[i]);
+		res += atoi(av[i]);
 		i++;
 	}
 	printf("%d\n", res);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
 int _putchar(char c);
 int _atoi(char *s);
 

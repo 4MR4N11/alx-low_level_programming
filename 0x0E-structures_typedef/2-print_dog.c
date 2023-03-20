e41e7c9d2b70d71f68b87d4bb76bd95b1b0ca32c
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-03-20 07:42:01 -0700

    Mandatory part

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,10 +13,10 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (!d->age)
+	if (d->age < 0)
 		printf("Age: (nil)\n");
 	else
-		printf("Age: %.1f\n", d->age);
+		printf("Age: %f\n", d->age);
 	if (!d->owner)
 		printf("Owner: (nil)\n");
 	else

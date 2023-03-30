5223c3a083ca28d49cdcf03091a1fa155ce6956c
4mr4n11 <amraniikhalid0606@gmail.com>
2023-03-30 17:33:06 +0000

    Advanced part

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_before_main - prints a string before main is executed
+ * Return: void
+ */
+
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

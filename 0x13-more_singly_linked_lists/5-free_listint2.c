4f54d117fb08d236b88b07977ab8e3957400b7c4
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-04-03 11:00:33 -0700

    fix task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -15,9 +15,9 @@ void free_listint2(listint_t **head)
 		return;
 	while (*head)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
 	}
 	*head = NULL;
 }

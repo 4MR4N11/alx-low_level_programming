cddd66ba4c64af593d7232887b4bea45470b5f7f
Khalid El Amrani <amraniikhalid0606@gmail.com>
2023-04-07 07:50:33 -0700

    Mandatory part

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,6 +7,7 @@
  * @letters: number of letters it should read and print.
  * Return: the actual number of letters it could read and print.
 */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,3 +14,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
+

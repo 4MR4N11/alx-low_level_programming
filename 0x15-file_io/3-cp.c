68a98f80daccb4d1735a48c84d67343a098ddc35
4mr4n11 <amraniikhalid0606@gmail.com>
2023-04-07 14:57:18 +0000

    Mandatory part

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,35 +1,6 @@
 #include "main.h"
 
 /**
- * _calloc - allocates memory for an array, same behaviour of calloc().
- * @nmemb: number of elements of an array.
- * @size: bytes of element.
- * Return: pointer to the new allocated memory area that sets to zero.
- * NULL if it fails.
- */
-
-void *_calloc(unsigned int nmemb, unsigned int size)
-{
-	int i;
-	int len;
-	char *ptr;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-	i = 0;
-	len = size * nmemb;
-	ptr = malloc(len);
-	if (!ptr)
-		return (NULL);
-	while (i < len)
-	{
-		ptr[i] = '\0';
-		i++;
-	}
-	return (ptr);
-}
-
-/**
  * fd_handle - closes two file descriptors.
  * @fd1: file descriptor 1.
  * @fd2: file descriptor 2.
@@ -96,7 +67,7 @@ int main(int ac, char **av)
 	fd1 = open(av[1], O_RDONLY);
 	fd2 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	fd_handle(fd1, fd2, 1, av);
-	buff = _calloc(BUFFER_SIZE, sizeof(char));
+	buff = malloc(sizeof(char) * BUFFER_SIZE);
 	while ((rsize = read(fd1, buff, BUFFER_SIZE)))
 	{
 		if (write(fd2, buff, rsize) < 0)
@@ -107,14 +78,15 @@ int main(int ac, char **av)
 			return (99);
 		}
 		free(buff);
-		buff = _calloc(BUFFER_SIZE, sizeof(char));
+		buff = malloc(sizeof(char) * BUFFER_SIZE);
 	}
-	free(buff);
 	if (rsize < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		free(buff);
 		fd_handle(fd1, fd2, 0, NULL);
 		return (98);
 	}
+	free(buff);
 	return (0);
 }

7f4a8ef71fc895883d502b6acecd0e8b4daee817
4mr4n11 <amraniikhalid0606@gmail.com>
2023-06-08 19:46:04 +0100

    Mandatory part

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,8 +13,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *node = NULL;
 	int index;
 
-	if (!key)
+	if (!key || !ht)
 		return (0);
+	
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);

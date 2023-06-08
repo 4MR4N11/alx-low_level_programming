966bcd83f19aa7fa880875a8cd956a0a351a5ec3
4mr4n11 <amraniikhalid0606@gmail.com>
2023-06-08 20:08:55 +0100

    Mandatory part

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look into
+ * @key: key to look for
+ * Return: value associated with the element, or NULL if key couldn't be found
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	
+}

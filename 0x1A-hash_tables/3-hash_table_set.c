a60371190bc861c5065d04b41a88d08c56269199
4mr4n11 <amraniikhalid0606@gmail.com>
2023-06-08 19:49:06 +0100

    Mandatory part

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,17 +11,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *node = NULL;
+	hash_node_t *tmp = NULL;
 	int index;
 
-	if (!key || !ht || !value)
+	if (!key || !ht)
 		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
 	node->key = (char *)key;
 	node->value = strdup(value);
 	node->next = NULL;
-	index = key_index((const unsigned char *)key, ht->size);
 	if (!ht->array[index])
 		ht->array[index] = node;
 	else

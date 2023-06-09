d69acbabdf912c5faf9954e83fdef811de8199e1
4mr4n11 <amraniikhalid0606@gmail.com>
2023-06-09 15:36:09 +0100

    Mandatory part

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to print
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long index = 0;
+	hash_node_t *tmp;
+	int check = 0; /*to check when should print the ", "*/
+
+	if (!ht)
+	{
+		printf("{}\n");
+		return;
+	}
+	printf("{");
+	while (index < ht->size)
+	{
+		tmp = ht->array[index];
+		while (tmp)
+		{
+			if (check)
+				printf(", ");
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			tmp = tmp->next;
+			check = 1;
+		}
+		index++;
+	}
+	printf("}\n");
+}
